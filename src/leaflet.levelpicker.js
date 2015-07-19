52868dcc27bc6ba13733d2c8eaa0d2485e68d1a8
christopherfricke <christopher.fricke@bmoregeo.com>
2015-07-18 21:14:22 -0400

    Throw in some comments

--- src/leaflet.levelpicker.js
+++ src/leaflet.levelpicker.js
@@ -11,38 +11,51 @@ L.Control.LevelPicker = L.Control.extend({
         selectedLevel: 1,
         enableLevelList: true
     },
-
+    /**
+     * Fires on init of controller
+     * @param options
+     */
     initialize: function (options) {
         L.Util.setOptions(this, options);
         this.levelListShowing = false;
         this.levelListButtons = [];
     },
-
+    /**
+     * Fires on add of controller to app
+     * @param map
+     * @returns {div}
+     */
     onAdd: function (map) {
-        // happens after added to map
         var self = this;
 
+        // Set level of map to default if not already set
         if (this._map.hasOwnProperty('level') || this._map.level == null){
             this._map.level = this.options.selectedLevel;
         }
+
+        // Initialize HTML elements
         var container = L.DomUtil.create('div', 'level-picker leaflet-bar');
         L.DomEvent.on(container, 'mousewheel', L.DomEvent.stopPropagation);
         L.DomEvent.disableClickPropagation(container);
 
+        // Add Up button
         this.upButton = L.DomUtil.create('a', 'level-picker-up', container);
         this.upButton.innerHTML = this.options.levelUpText;
         L.DomEvent.addListener(this.upButton, 'click', this.changeLevelUp, this);
 
+        // Add Level Info and add click event to show level info IF enableLevelList is set
         this.levelInfo = L.DomUtil.create('a', 'level-picker-level-info', container);
         if (this.options.enableLevelList){
             L.DomUtil.addClass(this.levelInfo, 'enable-level-list');
-            L.DomEvent.addListener(this.levelInfo, '', this.showLevelList, this);
+            L.DomEvent.addListener(this.levelInfo, 'click', this.showLevelList, this);
         }
 
+        // Add Down button
         this.downButton = L.DomUtil.create('a', 'level-picker-down', container);
         this.downButton.innerHTML = this.options.levelDownText;
         L.DomEvent.addListener(this.downButton, 'click', this.changeLevelDown, this);
 
+        // If Enable Level List is true then add all the levels to the level list container
         if (this.options.enableLevelList) {
             this.levelListContainer = L.DomUtil.create('div', 'level-list leaflet-bar', container);
             this.levelListButtonsContainer = L.DomUtil.create('div', 'pass', this.levelListContainer);
@@ -65,6 +78,12 @@ L.Control.LevelPicker = L.Control.extend({
 
         return container;
     },
+    /**
+     * Fires on removal of controller to app
+     * Remove All the click events on elements
+     * @param map
+     * @returns {div}
+     */
     onRemove: function (map) {
         L.DomEvent.removeListener(this.upButton, 'click', this.changeLevelUp, this);
         L.DomEvent.removeListener(this.levelInfo, 'click', this.showLevelList, this);
